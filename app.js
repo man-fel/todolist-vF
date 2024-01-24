c4e1cc330326032fcee45ff766111117d98e69c2
man-fel <96535824+man-fel@users.noreply.github.com>
2024-01-24 07:55:50 +0300

    Second commit

--- app.js
+++ app.js
@@ -4,7 +4,9 @@ const bodyParser = require("body-parser");
 
 const app= express();
 
-let items = ["",];
+let items = [];
+
+let workItems = [];
 
 app.set('view engine', 'ejs');
 
@@ -22,14 +24,27 @@ app.get("/", function(req, res){
        
     };
     let day = date.toLocaleDateString("en", options);
-    res.render("list", {kindOfDay: day, newListItems: items});
+    res.render("list", {listTitle: day, newListItems: items});
 })
 app.post("/", function(req, res){
     let item = req.body.newItem;
-    items.push(item);
-    res.redirect("/");
+
+    if (req.body.list === "Work"){
+        workItems.push(item);
+        res.redirect("/work")
+    }else{
+        items.push(item);
+        res.redirect("/")
+    }
 })
-   
+
+app.get("/work", function(req, res){
+    res.render("list", {listTitle: "Work List", newListItems: workItems})
+})
+
+app.get("/about", function(req, res){
+    res.render("about");
+})
 
 app.listen(3000, function(){
     console.log("Server started on port 3000");
